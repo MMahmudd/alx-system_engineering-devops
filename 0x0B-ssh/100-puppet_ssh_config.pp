2125e221499bc4c4b8843e076fa884f059ac73b5
MMahmudd <shametlab@gmail.com>
2023-08-26 09:52:46 +0000

    update tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
-# Configure_SSH to_use the_private key and_refuse password_authentication
+# Puppet_script to_create ssh config_file
 file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  ensure  => present,
-  require => File['/etc/ssh/ssh_config'],
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  ensure  => present,
-  require => File['/etc/ssh/ssh_config'],
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
