aafc3a12277c6ec2813adad4d170883d47074aab
MMahmudd <shametlab@gmail.com>
2023-09-25 03:25:09 -0700

    update task1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,12 @@ import sys
 
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
+    employee_Id = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    url = baseUrl + "/" + employee_id
 
     response = requests.get(url)
-    employeeName = response.json().get('name')
+    employee_Nme = response.json().get('name')
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             done += 1
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+          .format(employee_Nme, done, len(tasks)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
