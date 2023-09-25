0b2a14e752014a64737d6cfdfdd03c3d2722a7ae
MMahmudd <shametlab@gmail.com>
2023-09-25 03:19:55 -0700

    update task1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
+"""Accessing_a REST API for_todo lists_of_employees"""
 
 import requests
 import sys
+
 
 if __name__ == '__main__':
     employeeId = sys.argv[1]
@@ -11,9 +12,6 @@ if __name__ == '__main__':
 
     response = requests.get(url)
     employeeName = response.json().get('name')
-
-    # Truncate the employeeName to a maximum of 18 characters
-    employeeName = employeeName[:18] if len(employeeName) > 18 else employeeName
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
@@ -26,17 +24,8 @@ if __name__ == '__main__':
             done_tasks.append(task)
             done += 1
 
-    # Print employee name and to-do count in the expected format
-    print("Employee Name: OK" if len(employeeName) <= 18 else "Employee Name: Incorrect")
-    print("To Do Count: OK" if done == len(tasks) else "To Do Count: Incorrect")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
-    # Print first line formatting message in the expected format
-    print("First line formatting: OK" if len(employeeName) <= 18 and done == len(tasks) else "First line formatting: Incorrect")
-
-    # Print task titles
-    for i, task in enumerate(done_tasks, start=1):
-        print(f"Task {i} in output: OK")
-    
-    # Print task formatting message
-    for i in range(1, 13):
-        print(f"Task {i} Formatting: OK")
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
