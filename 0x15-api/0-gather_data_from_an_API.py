19ad5f125957034179a6df1fe650ae3dd48dffdf
MMahmudd <shametlab@gmail.com>
2023-09-25 03:45:12 -0700

    update tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,59 +1,27 @@
 #!/usr/bin/python3
-"""
-Accessing a REST API for todo lists_of_employees
-"""
-
-import sys
+'''
+Python_script that_returns information_using REST_API
+'''
 import requests
-
-def fetch_employee_todo_list(employee_id):
-    """
-    Fetches and displays an employee's TODO list progress.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    # Define the base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Create the URL for fetching employee information
-    employee_url = f"{base_url}/users/{employee_id}"
-
-    # Create the URL for fetching employee's TODO list
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        # Fetch employee information
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-
-        # Fetch TODO list for the employee
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        # Filter completed tasks
-        completed_tasks = [task for task in todos_data if task.get("completed")]
-
-        # Display the result
-        print(f"Employee {employee_data.get('name')} is done with tasks"
-              f"({len(completed_tasks)}/{len(todos_data)}):")
-        for task in completed_tasks:
-            print(f"\t{task.get('title')}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    except ValueError as e:
-        print(f"Error: Failed to parse JSON data - {e}")
-        sys.exit(1)
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    fetch_employee_todo_list(employee_id)
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
