898c0c342d0b7c2788f9bae704c07c1c82a9c50f
MMahmudd <shametlab@gmail.com>
2023-10-20 16:31:00 +0000

    update tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,12 @@
-# Increase_the_amount of_traffic an_nginx can_handle.
+# fix_limit_files.
 
-# Increase_the_ULIMIT of_the_defaul_file
-exec { 'fix--for--nginx':
-  # Modify_the_ULIMIT_value
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  # Specif0y_the_path for_sed_command
-  path    => '/etc/usr/local/bin/:/bin/',
+exec { 'fix_limit_hbton_user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Restart_Nginx
-exec { 'nginx-restart':
-  # Restart_Nginx_service
-  command => '/etc/init.d/nginx restart',
-  # Specify_the_path for_init.d scrpt
-  path    => '/etc/init.d/',
+# Increase_soft file_limit hbton ser.
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
